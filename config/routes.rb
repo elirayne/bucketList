96412ebaaa728e06d3f39f934652b4995d314d04
Jeremy Bird <jbird@jbird-l.corp.instructure.com>
2018-03-01 18:53:28 -0700

    update routes again...

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
 
   resources :lists do
     resources :destinations
+    resources :activities
   end
 
-    # best way for nested routes 3 deep
-    scope 'destinations/:destination_id', as: 'destination' do
-      resources :activities, only: [:new, :create]
-    end
+    # # best way for nested routes 3 deep
+    # scope 'destinations/:destination_id', as: 'destination' do
+    #   resources :activities, only: [:new, :create]
+    # end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

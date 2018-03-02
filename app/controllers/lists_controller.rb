288d11944d5f70c32311c61b0ef624eebdd3b3b6
Tiffany Bigelow <cbtiff@gmail.com>
2018-03-01 19:26:28 -0700

    list controller switched to lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,22 @@
 class ListsController < ApplicationController
   def index
+    @list = List.all
   end
 
   def show
-  end
-
-  def edit
+    @list = List.find(params[:id])
   end
 
   def new
+    @list = List.new
+  end
+
+  def edit
+    @List = List.find(params[:id])
+  end
+
+  def destroy
+    List.find(params[:id]).destroy
+    redirect_to lists_path
   end
 end
